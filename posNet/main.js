df79a481463b09b6f22ac3c7864545b0822e593f
dveschetti <56399809+dveschetti@users.noreply.github.com>
2020-07-23 22:49:03 +0200

    prova

--- posNet/main.js
+++ posNet/main.js
@@ -61,8 +61,8 @@ function draw() {
   //posLevelSmooth += (size.x - posLevelSmooth) * 0.09
   //lettera.style["font-size"] = 500 + "px"; 
   // blocca alla dimensione massima
-  if(size.x/5 > 301) {
-      lettera.style["font-size"] = 300 + "px"; 
+  if(size.x/5 > 401) {
+      lettera.style["font-size"] = 400 + "px"; 
   } else{
       lettera.style["font-size"] = size.x/5 + "px"; //dimensione varia alla distanza
   }
@@ -102,11 +102,11 @@ function draw() {
 
   //per trasformare la posizione e inserire la lettera centrale 
   //heightLetter deve avere lo stesso valore di "font-size"
-//   if (size.x/5 > 301){
-//   lettera.style.transform = "translateY(-" + 300 *0.7 + "px)";
-// } else {lettera.style.transform = "translateY(-" + heightLetter*0.7 + "px)";
+  if (size.x/5 > 401){
+  lettera.style.transform = "translateY(-" + 400 *0.7 + "px)";
+} else {lettera.style.transform = "translateY(-" + heightLetter*0.7 + "px)";
 
-// }
+}
   //console.log(w)
   //console.log(c)
 
