dd5b83fa66b8cfe43c8f6f4718e1b72ad20ed8c7
dveschetti <56399809+dveschetti@users.noreply.github.com>
2020-08-09 14:34:43 +0200

    created optotype folder
    riordinato le cartelle

---
+++ Optotype/js/home_main.js
@@ -0,0 +1,56 @@
+let $spacer = document.querySelector('.spacer');
+let $video = document.querySelector('.video');
+const lettera = document.getElementById("text")
+const buttonSkip = document.querySelector("#buttonSkip")
+
+// The height of the spacer element
+let spacerHeight = $spacer.clientHeight;
+// the height of the viewport
+let viewportHeight = document.documentElement.getBoundingClientRect().height;
+
+lettera.style["font-size"] = 100 + "px";
+
+// We can get the total scrollable height be subtracting the spacer element's height by the viewport height
+let scrollableHeight = spacerHeight - viewportHeight;
+// Get the full duration of the video
+let videoDuration;
+// Keep track the video's playtime
+let currentTime = 0;
+
+// The scroll event handler
+function handleScrollEvent(event) {
+  // Here we sync the y position of the scrollbar to the progress of the video
+  currentTime = window.scrollY * videoDuration / scrollableHeight;
+  $video.currentTime = currentTime;
+	const prova = Math.floor(map_range(currentTime,0,videoDuration,300,0))
+	console.log(currentTime,videoDuration, prova)
+	lettera.style["font-variation-settings"] = " 'SIZE' " + prova
+
+  console.log(document.documentElement.scrollTop,document.body.scrollTop)
+   if (document.body.scrollTop > 9064 || document.documentElement.scrollTop > 9064) {
+    const url = "index.html"
+    window.location = url
+  } 
+}
+
+// Loaded Data handler, that is, the function that runs after the video is ready to play
+function handleLoadedData(event) {
+  // Get the full video duration
+  videoDuration = $video.duration;
+  // Do stuff when user scrolls
+  window.addEventListener('scroll', handleScrollEvent);
+}
+
+// Do stuff when the video is ready to play
+$video.addEventListener('loadeddata', handleLoadedData);
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+buttonSkip.addEventListener("click", skipScroll)
+
+function skipScroll(){
+    const url = "index.html"
+    window.location = url
+}
---
+++ Optotype/js/index_main.js
@@ -0,0 +1,88 @@
+//seleziono il div con l'id text
+const lettera = document.getElementById("text")
+//seleziono il  div per  lo slider
+let Resolution = document.getElementById("Resolution")
+let Tracking = document.getElementById("Tracking")
+let Leading = document.getElementById("Leading")
+//aggiorno il valore dello slider con addEventListener
+Resolution.addEventListener('input', sliderChangeResolution);
+Tracking.addEventListener('input', sliderChangeTracking);
+Leading.addEventListener('input', sliderChangeLeading);
+
+const slidecontainer = document.querySelector("#slidecontainer")
+const buttonDistance = document.querySelector("#buttonDistance")
+const buttonSliders = document.querySelector("#buttonSliders")
+
+
+let output = document.getElementById("demo");
+output.innerHTML = Resolution.value;
+
+//dimensione font
+//lettera.style["font-size"] = "300px";
+
+
+const width = window.innerWidth
+const height = window.innerHeight
+
+// -- EVENTI ----------------------------------
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+//funzione che riprende il valore dello slider
+function sliderChangeResolution() {
+  lettera.style["font-variation-settings"] = " 'SIZE' " + this.value;
+  console.log(this.value);
+}
+
+function sliderChangeTracking() {
+  lettera.style["letter-spacing"] = this.value +"em";
+  console.log(this.value);
+}
+
+function sliderChangeLeading() {
+  lettera.style["line-height"] = this.value +"%";
+  console.log(this.value);
+}
+
+Resolution.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+
+
+// -- BOTTONI PRINCIPALI ------------------------------------------------
+buttonDistance.addEventListener("click", hide)
+
+function hide() {
+  slidecontainer.style.display = "none";
+}
+
+buttonSliders.addEventListener("click", show)
+
+function show() {
+  slidecontainer.style.display = "block";
+}
+
+
+// -- BOTTONE ABOUT ------------------------------------------------
+    const buttonAbout = document.querySelector("#buttonAbout")
+    const buttonInfo = document.querySelector("#info")
+    const about = document.querySelector("#about")
+    const buttonClose = document.querySelector("#buttonClose")
+    buttonAbout.addEventListener("click", showAbout)
+    info.addEventListener("click", showAbout)
+    buttonClose.addEventListener("click", hideAbout)
+
+    function showAbout() {
+        about.classList.add("active")
+        buttonAbout.style.display = "none";
+    }
+
+    function hideAbout() {
+        about.classList.remove("active")
+        buttonAbout.style.display = "inline-block";
+    }
+
