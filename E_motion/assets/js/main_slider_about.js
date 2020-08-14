70c473cba6c2ad374e284c57754623520537290c
chiara-crivelli <chiara.crivelli2012@gmail.com>
2020-08-14 22:01:57 +0200

    video

--- E_motion/assets/js/main_slider_about.js
+++ E_motion/assets/js/main_slider_about.js
@@ -1,73 +1,19 @@
-const letteraAbout = document.getElementById("text-about")
+const text = document.querySelector(".text");
+const sliders = document.querySelectorAll(".slider");
+const sliderValues = document.querySelectorAll(".output");
+// const buttons = document.querySelectorAll(".button");
 
-let SAJO = document.getElementById("SAJO")
-let DITR = document.getElementById("DITR")
-let ANFE = document.getElementById("ANFE")
-let ANSU = document.getElementById("ANSU")
-
-SAJO.addEventListener('input', sliderChangeSAJO);
-DITR.addEventListener('input', sliderChangeDITR);
-ANFE.addEventListener('input', sliderChangeANFE);
-ANSU.addEventListener('input', sliderChangeANSU);
-
-const slidecontainer = document.querySelector("#slidecontainer")
-const btnVoice = document.querySelector("#btnVoice")
-const btnMouse = document.querySelector("#btnMouse")
-const btnSlider = document.querySelector("#btnSliders")
-
-let outputSAJO = document.getElementById("demoSAJO");
-outputSAJO.innerHTML = SAJO.value;
-
-let outputDITR = document.getElementById("demoDITR");
-outputDITR.innerHTML = DITR.value;
-
-let outputANFE = document.getElementById("demoANFE");
-outputANFE.innerHTML = ANFE.value;
-
-let outputANSU = document.getElementById("demoANSU");
-outputANSU.innerHTML = ANSU.value;
-
-letteraAbout.style["font-size"] = "2rem";
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+// Display property values
+for (let i = 0; i < sliders.length; i++) {
+  sliderValues[i].innerHTML = sliders[i].value;
 }
 
-function sliderChangeSAJO() {
-  letteraAbout.style["font-variation-settings"] = " 'SAJO' " + this.value;
-  // console.log(this.value);
-}
-
-function sliderChangeDITR() {
-  letteraAbout.style["font-variation-settings"] = " 'DITR' " + this.value;
-  // console.log(this.value);
-}
-
-function sliderChangeANFE() {
-  letteraAbout.style["font-variation-settings"] = " 'ANFE' " + this.value;
-  // console.log(this.value);
-}
-
-function sliderChangeANSU() {
-  letteraAbout.style["font-variation-settings"] = " 'ANSU' " + this.value;
-  // console.log(this.value);
-}
-
-SAJO.oninput = function() {
-  outputSAJO.innerHTML = this.value;
-}
-
-DITR.oninput = function() {
-  outputDITR.innerHTML = this.value;
-}
-
-ANFE.oninput = function() {
-  outputANFE.innerHTML = this.value;
-}
-
-ANSU.oninput = function() {
-  outputANSU.innerHTML = this.value;
-}
+// Update text property and displayed property value for each slider
+sliders.forEach(slider => {
+    const sliderIndex = slider.getAttribute("data-index");
+    const output = document.querySelector(`.output[data-index="${sliderIndex}"]`);
+    slider.oninput = function() {
+      text.style.setProperty(`--${this.id}`, this.value);
+      output.innerHTML = this.value;
+    };
+  });
