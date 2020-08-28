1b6bdeeddf762e23bf472573a61cd93735b92fb5
chiara-crivelli <chiara.crivelli2012@gmail.com>
2020-08-28 20:51:20 +0200

    prova time line slider

--- E_motion/assets/js/script.js
+++ E_motion/assets/js/script.js
@@ -29,3 +29,19 @@ video.addEventListener('timeupdate', function(){
 		btn.className = "play";
 	}
 });
+
+
+var vid = document.getElementById("video");
+vid.ontimeupdate = function(){
+  var percentage = ( vid.currentTime / vid.duration ) * 100;
+  $("#custom-seekbar span").css("width", percentage+"%");
+};
+
+$("#custom-seekbar").on("click", function(e){
+    var offset = $(this).offset();
+    var left = (e.pageX - offset.left);
+    var totalWidth = $("#custom-seekbar").width();
+    var percentage = ( left / totalWidth );
+    var vidTime = vid.duration * percentage;
+    vid.currentTime = vidTime;
+});//click()
