723d91381fbcb03fe6c86a36d370c3b3d7f4f24b
chiara-crivelli <chiara.crivelli2012@gmail.com>
2020-08-28 21:26:44 +0200

    correzione time line

--- E_motion/assets/js/script.js
+++ E_motion/assets/js/script.js
@@ -1,36 +1,3 @@
-var video = document.querySelector('.video');
-var juice = document.querySelector('.orange-juice');
-var btn = document.getElementById('play-pause');
-
-function togglePlayPause(){
-	if(video.paused){
-		btn.className = "pause";
-		video.play();
-	} else {
-		btn.className = "play";
-		video.pause();
-	}
-}
-
-// btn.onclick = function() {
-// 	alert('play!!!');
-// 	togglePlayPause();
-// }
-
-	btn.onclick = function() {
-	// console.log("play!!!");
-	togglePlayPause();
-};
-
-video.addEventListener('timeupdate', function(){
-	var juicePos = video.currentTime / video.duration;
-	juice.style.width = juicePos * 100 + "%";
-	if (video.ended){
-		btn.className = "play";
-	}
-});
-
-
 var vid = document.getElementById("video");
 vid.ontimeupdate = function(){
   var percentage = ( vid.currentTime / vid.duration ) * 100;
@@ -45,3 +12,18 @@ $("#custom-seekbar").on("click", function(e){
     var vidTime = vid.duration * percentage;
     vid.currentTime = vidTime;
 });//click()
+
+function togglePlayPause(){
+	if(video.paused){
+		btn.className = "pause";
+		video.play();
+	} else {
+		btn.className = "play";
+		video.pause();
+	}
+}
+
+	btn.onclick = function() {
+	// console.log("play!!!");
+	togglePlayPause();
+};
