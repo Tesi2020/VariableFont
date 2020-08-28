2f1e2eaef14c8a501fc93522e2264a9502724b98
chiara-crivelli <chiara.crivelli2012@gmail.com>
2020-08-28 10:38:48 +0200

    font-size

--- E_motion/assets/js/main_smile.js
+++ E_motion/assets/js/main_smile.js
@@ -3,10 +3,10 @@ let micInit = false;
 let micLevelSmooth = 0;
 
 //seleziono la lettera dall'HTML
-const lettera = document.getElementById("demo-text")
+const lettera = document.getElementById("text")
 
 function setup(){
-  // createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth,windowHeight);
   mic = new p5.AudioIn()
   mic.start();
 }
@@ -21,7 +21,7 @@ function draw(){
   const d = micLevelSmooth * 50
 
   //dimesnione font
-  lettera.style["font-size"] = "200em";
+  lettera.style["font-size"] = "10px";
 
   //rimane in ascolto delle mosse del mouse e se ci fossero fanno quello che dice la funzione
   // document.addEventListener("mousemove", function(e){
